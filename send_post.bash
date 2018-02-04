daf54328fe82a441b130b208a30bad441b6d16a6
Anderson Lobo Feitosa <andersonlf@gmail.com>
2018-02-04 10:17:14 -0200

    big refactor

--- send_post.bash
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-#curl -d '{ "animal" : { "@class" : "com.andersonlfeitosa.jackson.Dog", "name" : "Anderson DOG", "barkVolume" : 2.0 }}' -H "Content-Type: application/json" -X POST http://localhost:8080/api1
-curl -d '{ "animal" : { "name" : "Anderson DOG", "barkVolume" : 2.0 }}' -H "Content-Type: application/json" -X POST http://localhost:8080/api1
-
--- send_post2.bash
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-curl -d '{ "name" : "anderson", "myProperty" : "myValue" }' -H "Content-Type: application/json" -X POST http://localhost:8080/api2
-
